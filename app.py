8cc224509f2f3f2e2feb21938c7d18348f17fa20
Shahabr99 <sean.rohani9@gmail.com>
2023-04-28 15:39:56 -0700

    almost done

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from models import db, connect_db, User
 
 
 app = Flask(__name__)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -20,13 +22,24 @@ def show_homepage():
     users = User.query.all()
     return render_template('home.html', users=users)
 
-@app.route('/result', methods=["POST"])
+@app.route('/form')
 def show_users():
+    return render_template('form.html')
+
+@app.route('/profile', methods=["POST"])
+def show_profile():
+
     first_name = request.form['first_name']
-    last_name= request.form['last-name']
+    last_name= request.form['last_name']
     image_url= request.form['image_url']
-    user = User(name=first_name, last_name=last_name, image_url=image_url)
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
 
     db.session.add(user)
     db.session.commit()
-    return redirect
+
+    return render_template('profile.html', firstname=first_name, lastname=last_name, image_url=image_url)
+
+
+
+
+
--- models.py
+++ models.py
@@ -12,13 +12,14 @@ class User(db.Model):
 
     __tablename__='users'
 
+    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    image_url=db.Column(db.Text, unique=True, nullable=True)
+
+
     def __repr__(self):
         """Show user infor"""
 
         p=self
         return f"<user {p.id} {p.first_name} {p.last_name} {p.image_url}"
-
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    first_name = db.Column(db.String(20), nullable=False)
-    last_name = db.Column(db.String(20), nullable=False)
-    image_url=db.Column(db.String(30), unique=True, nullable=True)
